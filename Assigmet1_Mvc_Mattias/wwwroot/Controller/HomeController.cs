846cbaf543fa8dbc4c1d0e6da7fc5c195bd01fcf
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-04-02 14:59:01 +0200

    Add project files.

---
+++ Assigmet1_Mvc_Mattias/wwwroot/Controller/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Assiigment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sledge()
+        {
+            return View();
+        }
+
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+    }
+}
